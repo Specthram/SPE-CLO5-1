5545edca8e258397c558447e2f86be2a6b5b638c
Anthony Ossent <frag132@gmail.com>
2017-04-01 21:19:01 +0200

    push base booking microservice

---
+++ BookingMicroService/src/routes/api.php
@@ -0,0 +1,69 @@
+<?php
+
+$app->get('/api/v1/booking', function(\Slim\Http\Request $request, \Slim\Http\Response$response, $args){
+
+	$routes = [];
+	$routes['list'][] = array("route" => "/booking/book", "method" => "GET", 'description' => 'gives all reserved rooms');
+	$routes['list'][] = array("route" => "/booking/book", "method" => "POST", 'description' => 'book a room (json -> ["room": "id_room"]), return 200 if ok or 403 if not');
+
+	$response->withStatus(200);
+	$response->write(json_encode($routes));
+
+	return $response;
+});
+
+$app->get('/api/v1/booking/test', function (\Slim\Http\Request $request, \Slim\Http\Response $response, $args){
+
+	$connection = new Cassandra\Connection(['127.0.0.1'], 'booking');
+
+	try {
+		$connection->connect();
+	} catch (Cassandra\Exception $e) {
+		echo 'Caught exception: ',  $e->getMessage(), "\n";
+		exit;
+	} finally {
+		echo 'ok ca marche';
+	}
+
+
+	//	$connection->queryAsync()
+});
+
+$app->get('/api/v1/booking/init', function(\Slim\Http\Request $request, \Slim\Http\Response$response, $args){
+	// Connecteur PDO_CASSANDRA
+	$db = new PDO('cassandra:host=127.0.0.1;port=9160');
+	$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	$db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+
+	// Création de l'espace de travail
+	$db->exec("CREATE KEYSPACE mytest with strategy_class = 'SimpleStrategy' and strategy_options:replication_factor=1;");
+});
+
+//TODO voir ça
+$app->get('/{keyspace}/{table}', function ($request, $response, $args) use ($app, $db) {
+	$db->setKeyspace($args['keyspace']);
+	$params = $request->getQueryParams();
+
+	$select = FluentCQL\Query::select('*')->from($args['table']);
+
+	if (!empty($params)){
+		$conditions = [];
+		foreach($params as $columnName => $value)
+			$conditions[] = $columnName . ' = ?';
+
+		$select->where(implode(' AND ', $conditions));
+	}
+
+	$preparedData = $select->prepare();
+
+	$bind = [];
+	$index = 0;
+	foreach($params as $value){
+		$bind[] = Cassandra\Type\Base::getTypeObject($preparedData['metadata']['columns'][$index]['type'], $value);
+		++$index;
+	}
+
+	$rows = $db->executeSync($preparedData['id'], $bind)->fetchAll();
+
+	$response->json($rows->toArray());
+});
