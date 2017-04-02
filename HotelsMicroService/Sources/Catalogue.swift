77c131349263733c0e45887c128df2d326f7123f
darknight <aloui.addel@gmail.com>
2017-04-02 22:42:23 +0200

    User service

--- HotelsMicroService/Sources/Catalogue.swift
+++ HotelsMicroService/Sources/Catalogue.swift
@@ -7,6 +7,7 @@
 //
 
 import PerfectHTTP
+import Darwin
 
 public class Catalogue {
     // Container for array of type Person
@@ -14,25 +15,42 @@ public class Catalogue {
     
     // Datafixture on init
     init(){
+        
         data = [
-            Hotel(name: "Le normandie", address: "12 rue du deauville, Deauville", phone: "0148866545", rating: 5, country: "France", description: "Hotel de la mort qui tue"),
-            Hotel(name: "Hilton hotel", address: "the fifht avenue, New York", phone: "555-456", rating: 5, country: "United State", description: "American hotel for best place ever in the big apple"),
-            Hotel(name: "Chez moi ça pue", address: "02 rue devillers, Pamiers", phone: "0915456841", rating: 1, country: "France", description: "Mieux vaut pas y séjourner")
+            Hotel(id:Int(arc4random()),name: "Le normandie", address: "12 rue du deauville, Deauville", phone: "0148866545", rating: 5, country: "France", description: "Hotel de la mort qui tue", room: [Room(id: 191, number: 19191, category:"SR", maxPerson: "3", price: "55", isBooked: true)]),
+            Hotel(id:Int(arc4random()),name: "Hilton hotel", address: "the fifht avenue, New York", phone: "555-456", rating: 5, country: "United State", description: "American hotel for best place ever in the big apple", room: [Room(id: 11, number: 191, category:"S", maxPerson: "4", price: "555", isBooked: false)]),
+            Hotel(id:Int(arc4random()),name: "Chez moi ça pue", address: "02 rue devillers, Pamiers", phone: "0915456841", rating: 1, country: "France", description: "Mieux vaut pas y séjourner", room: [Room(id: 9, number: 1, category:"CD", maxPerson: "5", price: "5500", isBooked: false)])
         ]
+        
+        setData(key: "Hilton hotel", value: convetToString(arr: [Hotel(id:Int(arc4random()),name: "Hilton hotel", address: "the fifht avenue, New York", phone: "555-456", rating: 5, country: "United State", description: "American hotel for best place ever in the big apple", room: [Room(id: 9, number: 1, category:"CD", maxPerson: "5", price: "5500", isBooked: true)])]))
+        
+        setData(key: "Le normandie", value: convetToString(arr: [Hotel(id:Int(arc4random()),name: "Le normandie", address: "12 rue du deauville, Deauville", phone: "0148866545", rating: 5, country: "France", description: "Hotel de la mort qui tue",room: [Room(id: 11, number: 191, category:"S", maxPerson: "4", price: "555", isBooked: false), Room(id: 9, number: 1, category:"CD", maxPerson: "5", price: "5500", isBooked: false)])]))
+        
+        setData(key: "aaa", value: convetToString(arr: [Hotel(id:Int(arc4random()),name: "Chez moi ça pue", address: "02 rue devillers, Pamiers", phone: "0915456841", rating: 1, country: "France", description: "Mieux vaut pas y séjourner", room: [])]))
+        
+        
+        
+        
     }
     
     public func list() -> String {
+        
         return toString()
     }
     
     public func add(_ request: HTTPRequest) -> String {
+        let i = Int(arc4random())
+        
         let new = Hotel(
+            id: i,
             name: request.param(name: "name")!,
             address: request.param(name: "address")!,
             phone: request.param(name: "phone")!,
             country: request.param(name: "country")!,
             description: request.param(name: "description")!
         )
+        
+        setData(key: String(i), value: convetToString(arr: [new]))
         data.append(new)
         return toString()
     }
@@ -41,6 +59,19 @@ public class Catalogue {
         var out = [String]()
         
         for m in self.data {
+            do {
+                out.append(try m.jsonEncodedString())
+            } catch {
+                print(error)
+            }
+        }
+        return "[\(out.joined(separator: ","))]"
+    }
+    
+    private func convetToString(arr: [Hotel]) -> String {
+        var out = [String]()
+        
+        for m in arr {
             do {
                 out.append(try m.jsonEncodedString())
             } catch {
--- HotelsMicroService/Sources/Hotel.swift
+++ HotelsMicroService/Sources/Hotel.swift
@@ -12,50 +12,60 @@ class Hotel : JSONConvertibleObject {
     
     static let registerName = "hotel"
     
+    var id: Int
     var name: String = ""
     var address: String = ""
     var phone: String = ""
     var rating: Int = 0
     var country: String = ""
     var description:String = ""
+    var room: [Room]?
     
     var location: String {
         return "\(address) \(country)"
     }
     
-    init(name: String, address: String, phone: String, country: String, description: String) {
+    init(id: Int, name: String, address: String, phone: String, country: String, description: String) {
+        self.id = id
         self.name	= name
         self.address	= address
         self.phone		= phone
         self.country	= country
-        self.description		= description
+        self.description = description
+        
     }
     
-    init(name: String, address: String, phone: String, rating: Int, country: String, description: String) {
+    init(id: Int, name: String, address: String, phone: String, rating: Int, country: String, description: String, room:[Room]?) {
+        self.id = id
         self.name	= name
         self.address	= address
         self.phone		= phone
         self.rating	= rating
         self.country	= country
-        self.description		= description
+        self.description = description
+        self.room = room
     }
     
     override public func setJSONValues(_ values: [String : Any]) {
+        self.id =  getJSONValue(named: "id", from: values, defaultValue: 1)
         self.name		= getJSONValue(named: "name", from: values, defaultValue: "")
         self.address		= getJSONValue(named: "address", from: values, defaultValue: "")
         self.phone			= getJSONValue(named: "phone", from: values, defaultValue: "")
         self.rating		= getJSONValue(named: "rating", from: values, defaultValue: 0)
         self.country			= getJSONValue(named: "country", from: values, defaultValue: "")
         self.description			= getJSONValue(named: "description", from: values, defaultValue: "")
+        self.room			= getJSONValue(named: "room", from: values, defaultValue: [])
     }
     override public func getJSONValues() -> [String : Any] {
         return [
+            "id": id,
             "name":name,
             "address":address,
             "phone":phone,
             "rating":rating,
             "country":country,
-            "description":description
+            "description":description,
+            "room":room as Any
         ]
     }
     
